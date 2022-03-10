c31c2c6c94030abfeae839aea2c43d1bad9ddd2e
Santaclaus <santa@northpole.com>
2022-03-10 16:07:14 +0300

    add array of strings

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let languages=['HTML', 'CSS', 'JavaScript'];
+languages.forEach(function(language){
+    //console.log('I Love ' + language + '!')
+   //alert('I Love '+ language +'!')
+})
+/*let total = 0;
+let digits = [1, 2, 3, 4, 5];
+digits.forEach(function(digit) {
+  total += digit;
+});
+alert(total); 
+console.log(total)
+let total = 0;
+let digits = [1, 2, 3, 4, 5];
+let x=digits.map(function(digit) {
+  return digit *2;
+});
+alert(x); 
+//console.log(x)
+let Numbers=[2,4,6,8,10,12]
+let y=Numbers.map(function(Number){
+    return Number+1;
+})
+//console.log(y)
+alert(y)
+let words=['art', 'music','biology','agric', 'chemistry']
+let uppercase=words.map(function(word){
+return word.toUpperCase()
+})
+//console.log(uppercase)
+console.log(words)*/
+
+let friends=['Collo','Faith','Alex','Pauline']
+friends.forEach(function(friend){
+alert('Hello good evening '+ friend)
+})
+
+
